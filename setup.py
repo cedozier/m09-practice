4cf1f97faab7dab4ef4149d4570e582ffef67f71
ClaireElaine_Dozier <cedozier9274@gmail.com>
2025-03-18 20:06:28 -0400

    Done.

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Demo',
+    version='0.1',
+    description='A simple demonstration package',
+    url='http://github.com/cedozier/m09-practice',
+    author='Claire Dozier',
+    author_email='cedozier9274@gmail.com',
+    license='MIT',
+    packages=['demo']
+)
